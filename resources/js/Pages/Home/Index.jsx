8e3e15cc75fca55c39edeb1ec8abbb13dc277c7f
adamstrange <adamstrange@sictamil.com>
2024-11-19 19:14:35 +0200

    react setup - Home page draft done

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,15 +1,60 @@
-import React, { memo, useCallback, useEffect, useState } from 'react'
+import React, {memo, useCallback, useEffect, useState} from 'react'
+import {Button, Icon, Paper, Typography} from "@mui/material";
+import * as yup from 'yup'
+import {Formik, Field, Form} from "formik";
+import {TextField} from 'formik-mui';
 // import { withRouter } from 'react-router'
+import Box from '@mui/material/Box';
 
-const HomeIndex = ({ enqueueSnackbar, history }) => {
+const HomeIndex = ({enqueueSnackbar, history}) => {
+    const [uniqueLink, setUniqueLink] = useState(null)
+
+    const values = {
+        userName: '',
+        phoneNumber: '',
+    }
+
+    const validationSchema = () => {
+        const schema = {
+            userName: yup
+                .string()
+                .required()
+                .label('Username'),
+            phoneNumber: yup
+                .string()
+                .required()
+                .label('Phonenumber'),
+        }
+
+        return yup.object().shape(schema)
+    }
+
+    const onSubmit = useCallback(values => {
+        setUniqueLink('--UNIQUE LINK GENERATED--')
+        console.log('Submit')
+    }, [history, enqueueSnackbar])
+
+
     return <>
-        <h2 className="text-xl font-semibold text-black dark:text-white">Home Page</h2>
-
-        <p className="mt-4 text-sm/relaxed">
-            Laravel has wonderful documentation covering every aspect of the framework. Whether
-            you are a newcomer or have prior experience with Laravel, we recommend reading our
-            documentation from beginning to end.
-        </p>
+        <h2 className="text-xl font-semibold text-black dark:text-white">Generate unique link</h2>
+        {!uniqueLink && (
+            <Formik
+                initialValues={values}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+                // onSubmit={async (values) => {
+                //     await new Promise((resolve) => setTimeout(resolve, 500));
+                //     alert(JSON.stringify(values, null, 2));
+                // }}
+            >
+                <Form>
+                    <Field component={TextField} name="userName" label="Username" type="text"/>
+                    <Field component={TextField} name="phoneNumber" label="Phonenumber" type="text"/>
+                    <Button type="submit" variant="contained" color="primary">Register</Button>
+                </Form>
+            </Formik>
+        )}
+        {uniqueLink ? uniqueLink : null}
     </>
 }
 
