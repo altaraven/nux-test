7ef93f5e1da2caf59de7b502f56eeb2312514d74
adamstrange <adamstrange@sictamil.com>
2024-11-21 18:23:43 +0200

    Game page - pt.4 - strategy finished

--- app/Game/Strategies/FromOneToThousandStrategy.php
+++ app/Game/Strategies/FromOneToThousandStrategy.php
@@ -4,10 +4,34 @@ declare(strict_types=1);
 
 namespace App\Game\Strategies;
 
+use Brick\Math\BigDecimal;
+use Random\Randomizer;
+
 class FromOneToThousandStrategy implements GameStrategy
 {
+    private const DICE_MIN = 1;
+    private const DICE_MAX = 1000;
+    private const AMOUNT_PRECISION = 2;
+
     public function play(): GameResultDto
     {
-        return new GameResultDto(1, false, 0.00);
+        $amount = 0;
+        $diceResult = (new Randomizer())->getInt(self::DICE_MIN, self::DICE_MAX);
+        $isWin = !($diceResult % 2);
+        if ($isWin) {
+            $percentage = match (true) {
+                $diceResult > 900 => 70,
+                $diceResult > 600 => 50,
+                $diceResult > 300 => 30,
+                $diceResult <= 300 => 10,
+            };
+
+            $amount = BigDecimal::of($diceResult)
+                ->multipliedBy($percentage)
+                ->dividedBy(100, self::AMOUNT_PRECISION)
+                ->toFloat();
+        }
+
+        return new GameResultDto($diceResult, $isWin, $amount);
     }
 }
